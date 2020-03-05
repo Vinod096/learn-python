0bf26bb4c754e733b6d9db517eaa42a00f528475
vinod096 <vinod.raipati@hotmail.com>
2020-03-05 18:17:37 +1100

    MPG is calculated

---
+++ lesson_02/personal/miles.py
@@ -0,0 +1,6 @@
+#MPG = Miles driven Gallons of gas
+miles = float(input("enter the miles drived : "))
+gas = float(input("enter the gas used for travelling : "))
+MPG = miles / gas
+print("MPG is : ",MPG)
+
