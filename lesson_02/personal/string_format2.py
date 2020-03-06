d16463d0f702e3b8987415db32011a814a66031d
vinod096 <vinod.raipati@hotmail.com>
2020-03-06 18:40:09 +1100

    string format 2

--- lesson_02/personal/string_format2.py
+++ lesson_02/personal/string_format2.py
@@ -5,4 +5,4 @@ Roll_number = 10001
 print("Hi, my name is {0} and my roll number is {1}".format(name,Roll_number))
 print(f"Hi, my name is {name} and my roll number is {Roll_number}")
 print("Hi," " my name is",name,"and my roll number is",Roll_number)
-print("Hi," " my name is "+name+" and my roll nymber is "+str(Roll_number))
+print("Hi," " my name is "+name+" and my roll number is "+str(Roll_number))
