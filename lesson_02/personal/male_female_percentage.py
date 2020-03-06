a4e004648eff870c1b11587cdfa2c3dd614d3c1f
vinod096 <vinod.raipati@hotmail.com>
2020-03-06 18:51:49 +1100

    percentage of students calculated

---
+++ lesson_02/personal/male_female_percentage.py
@@ -0,0 +1,10 @@
+males = int(input("enter the number of males : "))
+print("Number of males :",males)
+females = int(input("enter the number of females : "))
+print("Number of females :",females)
+total = males + females
+print("total students in class :",total)
+percentage_of_males = males/total
+print("percentage of males :",percentage_of_males)
+percentage_of_females = females/total
+print("percentage of females :",percentage_of_females)
