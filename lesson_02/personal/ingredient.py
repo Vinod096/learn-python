57639645835651b8851c5c564ca15792acab6b88
vinod096 <vinod.raipati@hotmail.com>
2020-03-07 00:52:44 +1100

    modified

--- lesson_02/personal/ingredient.py
+++ lesson_02/personal/ingredient.py
@@ -1,20 +1,21 @@
-Total_no_of_cookies = 48
+total_no_of_cookies = 48
 cups_of_sugar = 1.5
 cups_of_butter = 1
 cups_of_flour = 2.75
-#**********************************************#
+no_of_cookies_req = round(float(input("cookies required :")))
+print("no of cookies required in roundup : {0:2f} ".format(no_of_cookies_req))
+req_sugar = (no_of_cookies_req / total_no_of_cookies) *  cups_of_sugar
+print("no of cups of sugar_required  : {0:.2f}".format(req_sugar))
+req_butter = (no_of_cookies_req / total_no_of_cookies) * cups_of_butter
+print("no of cups of butter_required : {0:.2f}".format(req_butter))
+flour_req = (no_of_cookies_req / total_no_of_cookies) * cups_of_flour
+print("no of cups of flour_required : {0:.2f}".format(flour_req))
 
-print("Given values are :- ")
-print("Total no of cookies =",Total_no_of_cookies)
-print("cups of sugar =",cups_of_sugar)
-print("cups of butter =",cups_of_butter)
-print("cups of flour =",cups_of_flour)
-
-#***********************************************#
-No_of_cookies_Req = float(input("cookies required :"))
-ReqSugar = (No_of_cookies_Req / Total_no_of_cookies) *  cups_of_sugar
-print("No of cups of SugarReq :",ReqSugar)
-ReqButter = (No_of_cookies_Req / Total_no_of_cookies) * cups_of_butter
-print("No of cups of ButterReq :",ReqButter)
-FlourReq = (No_of_cookies_Req / Total_no_of_cookies) * cups_of_flour
-print("No of cups of FlourReq :",FlourReq)
+#Question :
+#A cookie recipe calls for the following ingredients:
+#• 1.5 cups of sugar
+#• 1 cup of butter
+#• 2.75 cups of flour
+#The recipe produces 48 cookies with this amount of the ingredients. Write a program that
+#asks the user how many cookies he or she wants to make, and then displays the number of
+#cups of each ingredient needed for the specified number of cookies.
