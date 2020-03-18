2a8dab743c203066c696327d107b5b509f4ccab7
vinod096 <vinod.raipati@hotmail.com>
2020-03-18 20:56:11 +1100

    for loop

--- lesson_02/personal/For_loop/scrimba.py
+++ lesson_02/personal/For_loop/scrimba.py
@@ -2,7 +2,7 @@ names = ['Jane','John','Alena']
 names_1 = ['John','Jane','Adeev']
 text = 'Welcome to the party'
 names.extend(names_1)
-for name in range(3):
+for name in range(1):
     names.append(input("enter name :"))
 for name in names:
     print(name,text)
