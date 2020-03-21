e90940b5406f601cc79c4b934f8acf90ea89f518
Vinod096 <61657451+Vinod096@users.noreply.github.com>
2020-03-22 00:04:11 +1100

    Delete miles.py

--- lesson_02/personal/miles.py
+++
@@ -1,6 +0,0 @@
-#MPG = Miles driven Gallons of gas
-miles = float(input("enter the miles drived : "))
-gas = float(input("enter the gas used for travelling : "))
-MPG = miles / gas
-print("MPG is : ",MPG)
-
