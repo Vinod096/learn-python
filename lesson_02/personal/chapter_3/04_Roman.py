570392a3c4f572f76f23f5c6f36b2daef6764ade
vinod096 <vinod.raipati@hotmail.com>
2020-03-22 13:30:55 +1100

    Roman numbers

--- lesson_02/personal/chapter_3/04_Roman.py
+++ lesson_02/personal/chapter_3/04_Roman.py
@@ -13,3 +13,27 @@
 #8          VIII
 #9          IX
 #10         X
+
+number = int(input("enter a number :"))
+if number == 1:
+    print("Roman number of 1 : I")
+elif number == 2:
+    print("Roman number of 2 : II")
+elif number == 3:
+    print("Roman number of 3: III")
+elif number == 4:
+    print("Roman number of 4 : IV")
+elif number == 5:
+    print("Roman number of 5 : V")
+elif number == 6:
+    print("Roman number of 6 : VI")
+elif number == 7:
+    print("Roman number of 7 : VII")
+elif number == 8:
+    print("Roman number of 8 : VIII")
+elif number == 9:
+    print("Roman number of 9 : IX")
+elif number == 10:
+    print("Roman number of 10 : X")
+else:
+    print("please enter below 10 or greater the 1")
