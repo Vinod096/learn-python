bd7dc9a990645bb14ec5672d5db999c02629d23c
vinod096 <vinod.raipati@hotmail.com>
2020-03-22 12:57:03 +1100

    rectangle areas

--- lesson_02/personal/chapter_3/02_rectangle.py
+++ lesson_02/personal/chapter_3/02_rectangle.py
@@ -1,3 +1,19 @@
 #The area of a rectangle is the rectangle’s length times its width. Write a program that asks
 #for the length and width of two rectangles. The program should tell the user which rectangle
 #has the greater area, or if the areas are the same.
+
+length_of_rectangle_1 = int(input("Enter a number : "))
+width_of_rectangle_1 = int(input("Enter a number : "))
+area_of_rectangle_1 = length_of_rectangle_1 * width_of_rectangle_1
+print("area_of_rectangle_1 :",area_of_rectangle_1)
+length_of_rectangle_2 = int(input("Enter a number : "))
+width_of_rectangle_2 = int(input("Enter a number : "))
+area_of_rectangle_2 = length_of_rectangle_2 * width_of_rectangle_2
+print("area_of_rectangle_2 :",area_of_rectangle_2)
+if area_of_rectangle_1 == area_of_rectangle_2:
+    print("area of two rectangles have same area")
+elif area_of_rectangle_1 >= area_of_rectangle_2:
+    print("area of rectangel 1 has greater area")
+else:
+    print("area of rectangle 2 has greater area")
+
