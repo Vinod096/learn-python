af373b01ce1473f3d3de50e6ec0fc04b8f9451c5
vinod096 <vinod.raipati@hotmail.com>
2020-03-26 18:41:31 +1100

    calories burning

---
+++ lesson_02/personal/chapter_4/02_calories.py
@@ -0,0 +1,9 @@
+#Running on a particular treadmill you burn 4.2 calories per minute. Write a program that
+#uses a loop to display the number of calories burned after 10, 15, 20, 25, and 30 minutes.
+
+calories_burn_per_minute = 4.2
+minutes = 0
+for i in range (0,minutes+1):
+    minutes = int(input("enter number of minutes :"))
+    calories_burn_per_minute = calories_burn_per_minute * minutes
+print("calories burned :",calories_burn_per_minute)
