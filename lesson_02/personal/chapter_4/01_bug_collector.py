093d0247c5864ada5cb595104ddc3fe9b09601b8
vinod096 <vinod.raipati@hotmail.com>
2020-03-26 18:21:06 +1100

    bugs collector

---
+++ lesson_02/personal/chapter_4/01_bug_collector.py
@@ -0,0 +1,13 @@
+#A bug collector collects bugs every day for five days. Write a program that keeps a running
+#total of the number of bugs collected during the five days. The loop should ask for the
+#number of bugs collected for each day, and when the loop is finished, the program should
+#display the total number of bugs collected.
+
+total_days = 5
+total_bugs = 0
+for i in range(1, total_days + 1):
+    bugs_collected = int(input("enter number of bugs collected per day  :"))
+    print("bugs collected per day :", bugs_collected)
+    total_bugs = total_bugs + bugs_collected
+print("total bugs collected for 5 days :",total_bugs)
+
