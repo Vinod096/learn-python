20f3b1ca3645fa2aaa4208aa0e626008a81b3e21
vinod096 <vinod.raipati@hotmail.com>
2020-03-27 19:42:29 +1100

    tuition fee wrong output

---
+++ lesson_02/personal/chapter_4/10_tuition.py
@@ -0,0 +1,13 @@
+#At one college, the tuition for a full-time student is $8,000 per semester. It has been
+#announced that the tuition will increase by 3 percent each year for the next 5 years. Write
+#a program with a loop that displays the projected semester tuition amount for the next 5
+#years.
+
+tuition_fee_per_sem = 8000
+percent_of_fee_hike = 0.03
+total_fee_per_year = 0
+years = 5
+for years in range (1,years + 1):
+    tuition_fee_per_sem = (tuition_fee_per_sem * percent_of_fee_hike) + tuition_fee_per_sem
+    total_fee_per_year = total_fee_per_year + tuition_fee_per_sem
+    print(f"fee for {years} year : {total_fee_per_year}")
