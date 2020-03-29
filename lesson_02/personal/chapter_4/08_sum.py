3c4c3d6b3d1bd7a13cb08d05d4b415ab420befa8
vinod096 <vinod.raipati@hotmail.com>
2020-03-29 17:16:42 +1100

    .\lesson_02\personal\chapter_4\08_sum.py

---
+++ lesson_02/personal/chapter_4/08_sum.py
@@ -0,0 +1,11 @@
+#Write a program with a loop that asks the user to enter a series of positive numbers. The
+#user should enter a negative number to signal the end of the series. After all the positive
+#numbers have been entered, the program should display their sum.
+
+total = 0
+number = int(input("enter a positive number :"))
+while number > -1 :
+    total = total + number
+    number = int(input("enter a positive number or negative number to end :"))
+print("total of postive numbers is :",total)
+
