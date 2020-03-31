6a5a961fa7a362287b2fdc19f78bbe33777a7518
vinod096 <vinod.raipati@hotmail.com>
2020-03-31 18:16:59 +1100

    miles convertor

---
+++ lesson_02/personal/chapter_5/01_km_convertor.py
@@ -0,0 +1,7 @@
+#Write a program that asks the user to enter a distance in kilometers, and then converts that
+#distance to miles. The conversion formula is as follows:
+#Miles = Kilometers * 0.6214
+
+kilometers = float(input("enter kilometers :"))
+miles = kilometers * 0.6214
+print("distance travelled in miles :{0:2f}".format(miles))
