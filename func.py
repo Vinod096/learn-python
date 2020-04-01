e84e43fd2971cb73bfa565253ecd0195f7610ca6
vinod096 <vinod.raipati@hotmail.com>
2020-04-01 23:22:07 +1100

    function sample

---
+++ func.py
@@ -0,0 +1,13 @@
+def display_name(name):
+    """
+    Takes name as input and greets with hello
+    >>> display_name("John")
+    Hello John
+    >>> display_name("Joe")
+    Hello Joe
+    """
+    print("Hello", name)
+
+display_name("Joe")
+
+print("Hello")
