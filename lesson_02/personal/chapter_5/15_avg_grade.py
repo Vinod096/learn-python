5670dbf5a4d367cb514c6c325b4770c887721257
vinod096 <vinod.raipati@hotmail.com>
2020-04-05 18:13:13 +1000

    average and grade

--- lesson_02/personal/chapter_5/15_avg_grade.py
+++ lesson_02/personal/chapter_5/15_avg_grade.py
@@ -12,43 +12,67 @@
 #60–69          D
 #Below 60       F
 
+number_of_subjects = 5
+max_marks_for_subject = abs(float(input("enter max_marks :")))
+total_marks = max_marks_for_subject * 5
 
 def subjects():
-    print("Enter 5 subjets :")
-    for i in range (1,6):
-        number_of_subjects = int(input(f"enter {i} subject : "))
-        sub_total = sub_total + number_of_subjects
+    subject_one = abs(float(input("enter the marks for subject_1 : ")))
+    if subject_one <= max_marks_for_subject and subject_one != max_marks_for_subject:
+        print("in range")
+    else:
+        print("please enter below max_marks i.e :", max_marks_for_subject)
+#----------------------------------------------------#
+    subject_two = abs(float(input("enter the marks for subject_2 : ")))
+    if subject_two <= max_marks_for_subject and subject_two != max_marks_for_subject:
+        print("in range")
+    else:
+        print("please enter below max_marks i.e :", max_marks_for_subject)
+#----------------------------------------------------#
+    subject_three = abs(float(input("enter the marks for subject_3 : ")))
+    if subject_three <= max_marks_for_subject and subject_three != max_marks_for_subject:
+        print("in range")
+    else:
+        print("please enter below max_marks i.e :", max_marks_for_subject)
+#----------------------------------------------------#
+    subject_four = abs(float(input("enter the marks for subject_4 : ")))
+    if subject_four <= max_marks_for_subject and subject_four != max_marks_for_subject:
+        print("in range")
+    else:
+        print("please enter below max_marks i.e :", max_marks_for_subject)
+#----------------------------------------------------#
+    subject_five = abs(float(input("enter the marks for subject_5: ")))
+    if subject_five <= max_marks_for_subject and subject_five != max_marks_for_subject:
+        print("in range")
+    else:
+        print("please enter below max_marks i.e :", max_marks_for_subject)
+    sub_total = subject_one + subject_two + subject_three + subject_four + subject_five
     return sub_total
 
 def average(sub_total, number_of_subjects):
     avg = sub_total / number_of_subjects
     return avg
 
-def percent(sub_total, number_of_subjects):
-    total_marks = 0
-    maximum_marks_for_subject = 0
-    maximum_marks_for_subject = int(input("Enter maximum marks for subject : "))
-    total_marks = maximum_marks_for_subject * number_of_subjects
+def percentage_of_marks(sub_total,total_marks):
     percentage = (sub_total / total_marks) * 100
-    if percentage <= 100 and percentage >= 90:
+    if percentage < 100 and percentage > 90:
         print("A")
-    if percentage <= 89 and percentage >= 80:
+    if percentage < 89 and percentage > 80:
         print("B")
-    if percentage <= 79 and percentage >= 70:
-       print("C")
-    if percentage <= 69 and percentage >= 60:
+    if percentage < 79 and percentage > 70:
+        print("C")
+    if percentage < 69 and percentage > 60:
         print("D")
-    if percentage < 60 and percentage >= 0:
+    if percentage < 60 :
         print("F")
     return percentage
 
 def main():
-    total_of_subjects = subjects()
-    avg_of_subjects = average(total_of_subjects)
-    percentage_of_marks = percent(total_of_subjects)
-    print("total of subjects :",total_of_subjects)
-    print("avgerage of subjects :",avg_of_subjects)
-    print("percentage and grades :",percentage_of_marks)
+    subjects_total = subjects()
+    avg_of_subjects = average(subjects_total,number_of_subjects)
+    marks_percentage = percentage_of_marks(subjects_total,total_marks)
+    print("total of 5 subjects : ",subjects_total)
+    print("average marks :",avg_of_subjects)
+    print("percentage :",marks_percentage)
 
 main()
-
