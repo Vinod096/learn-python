002e35556b29f71a50c751ae79c7a87b0dec5e35
vinod096 <vinod.raipati@hotmail.com>
2020-05-02 21:56:53 +1000

    random numbers

---
+++ lesson_02/personal/chapter_6/07_random_numbers.py
@@ -0,0 +1,12 @@
+#Write a program that writes a series of random numbers to a file. Each random number
+#should be in the range of 1 through 500. The application should let the user specify how
+#many random numbers the file will hold.
+
+import random
+
+ran_numbers = random.randint(1,500)
+user = int(input("How many random numbers in the file : "))
+file = open("random_number.txt","w")
+for i in range (1, user + 1):
+    i += user + 1
+    print("Random numbers :",i)
