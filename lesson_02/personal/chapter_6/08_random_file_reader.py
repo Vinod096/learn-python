1bb6b45d22d82268407c0165868b0f6240099869
vinod096 <vinod.raipati@hotmail.com>
2020-05-03 16:33:35 +1000

    random numbers files reader

---
+++ lesson_02/personal/chapter_6/08_random_file_reader.py
@@ -0,0 +1,27 @@
+#This exercise assumes you have completed Programming Exercise 7, Random Number File
+#Writer. Write another program that reads the random numbers from the file, display the
+#numbers, and then display the following data:
+#• The total of the numbers
+#• The number of random numbers read from the file
+
+count = 0
+files = open("random_number.txt", "r")
+def ran_numbers_in_file():
+    count = files.readlines()
+    number_of_items = len(count)
+    return number_of_items
+
+def sum_of_numbers():
+    total = 0
+    for total in files:
+        number = int(files.readline())
+        total += number
+    return total
+
+def main():
+    count_of_ran_numbers = ran_numbers_in_file()
+    Numbers_total = sum_of_numbers()
+    print("Number of random numbers :",count_of_ran_numbers)
+    print("Total of random numbers :",Numbers_total)
+
+main()
