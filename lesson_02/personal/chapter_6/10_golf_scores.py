7e7a0a5c33f856915882b648bd6a21af653da0f4
vinod096 <vinod.raipati@hotmail.com>
2020-05-03 17:02:34 +1000

    golf scores

--- lesson_02/personal/chapter_6/10_golf_scores.py
+++ lesson_02/personal/chapter_6/10_golf_scores.py
@@ -2,7 +2,6 @@
 #1. A program that will read each player’s name and golf score as keyboard input, and thensave these as records in a file named golf.txt. (Each record will have a field for the player’s name and a field for the player’s score.)
 #2. A program that reads the records from the golf.txt file and displays them.
 
-
 while (True):
     golf_club = open("golf.txt", "w")
     player_name = str(input("Enter player name : "))
