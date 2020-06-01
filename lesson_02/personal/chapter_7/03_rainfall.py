890fe1e565f3db10abae828faa709450b0895315
vinod096 <vinod.raipati@hotmail.com>
2020-06-01 13:02:20 +1000

    rainfall

--- lesson_02/personal/chapter_7/03_rainfall.py
+++ lesson_02/personal/chapter_7/03_rainfall.py
@@ -2,19 +2,13 @@
 #list. The program should calculate and display the total rainfall for the year, the average
 #monthly rainfall, and the months with the highest and lowest amounts.
 
-total_months = 12
-rainfall_in_each_month = [0] * 12
-#print(rainfall_in_each_month)
-index = 0
-total = 0
-while index < total_months:
-    print('month -- ', index + 1, ':', sep='', end='')
-    rainfall_in_each_month[index] = float(input())
-    index += 1
-for Value in rainfall_in_each_month:
-    total += Value
-print("total rainfall in a year is :",total)
-average = total / len(rainfall_in_each_month)
+rainfall_in_a_year = []
+for i in range (1,13):
+    rainfall_in_each_month = float(str(input(f"Enter month {i} rainfall :")))
+    rainfall_in_a_year.append(rainfall_in_each_month)
+total = sum(rainfall_in_a_year)
+print("total rainfall in a year :",total)
+average = total / len(rainfall_in_a_year)
 print("average rainfall in a year is :",average)
-print("Maximum rainfall in a year :",max(rainfall_in_each_month))
-print("Minimum rainfall in a year :",min(rainfall_in_each_month))
+print("Maximum rainfall in a year :",max(rainfall_in_a_year))
+print("Minimum rainfall in a year :",min(rainfall_in_a_year))
