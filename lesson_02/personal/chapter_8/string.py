4b0f2009f0e956af3e14e7e744839dee3e1ef8a2
vinod096 <vinod.raipati@hotmail.com>
2020-06-01 13:07:13 +1000

    string introduction

---
+++ lesson_02/personal/chapter_8/string.py
@@ -0,0 +1,25 @@
+# hell =int [10] = "hello, World\0"
+
+
+# #include<stdio.h>
+
+# int len(char str[]){
+#     int len = 0
+#     for(int i=0;str[i] != '\0';i++){
+#         len++
+#     }
+#    return len;
+# }
+
+hello = "Hello, World!!"
+
+print(hello[0])
+
+arr = [10,2,3,4,5]
+
+for i in range(len(arr)):
+    print(arr[i])
+    print("-----")
+    print(i)
+
+
