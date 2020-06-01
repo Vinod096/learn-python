8d07dea5c9dfb096bb89811e28391c73a33db823
vinod096 <vinod.raipati@hotmail.com>
2020-06-01 12:49:45 +1000

    translator

---
+++ lesson_02/personal/chapter_8/05_translator.py
@@ -0,0 +1,17 @@
+#Many companies use telephone numbers like 555-GET-FOOD so the number is easier for
+#their customers to remember. On a standard telephone, the alphabetic letters are mapped
+#to numbers in the following fashion:
+#A, B, and C = 2
+#D, E, and F = 3
+#G, H, and I = 4
+#J, K, and L = 5
+#M, N, and O = 6
+#P, Q, R, and S = 7
+#T, U, and V = 8
+#W, X, Y, and Z = 9
+#Write a program that asks the user to enter a 10-character telephone number in the format
+#XXX-XXX-XXXX. The application should display the telephone number with any
+#alphabetic characters that appeared in the original translated to their numeric equivalent.
+#For example, if the user enters 555-GET-FOOD the application should display
+#555-438-3663.
+
